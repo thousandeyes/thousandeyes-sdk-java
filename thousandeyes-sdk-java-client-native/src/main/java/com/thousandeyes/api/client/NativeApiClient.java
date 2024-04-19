1fd9adeb52c2ca977990e283d6b72c115ff91214
Jack Browne <jabrowne@thousandeyes.com>
2024-04-19 12:26:34 +0100

    CP-2085 Update ApiClient to handle list responses

--- thousandeyes-sdk-java-client-native/src/main/java/com/thousandeyes/api/client/NativeApiClient.java
+++ thousandeyes-sdk-java-client-native/src/main/java/com/thousandeyes/api/client/NativeApiClient.java
@@ -6,13 +6,17 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.StringJoiner;
 import java.util.function.Consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+
 
 @AllArgsConstructor
 public class NativeApiClient implements ApiClient {
@@ -33,6 +37,21 @@ public class NativeApiClient implements ApiClient {
 
     @Override
     public <T> ApiResponse<T> send(ApiRequest request, Class<T> returnType) throws ApiException {
+        return sendRequestAndProcessResponse(
+                getRequestBuilder(request),
+                mapper.readerFor(returnType));
+    }
+
+    @Override
+    public <T> ApiResponse<List<T>> sendForList(ApiRequest request, Class<T> returnType)
+            throws ApiException
+    {
+        return sendRequestAndProcessResponse(
+                getRequestBuilder(request),
+                mapper.readerForListOf(returnType));
+    }
+
+    private HttpRequest.Builder getRequestBuilder(ApiRequest request) throws ApiException {
         var httpRequest = HttpRequest.newBuilder();
 
         if (request.getReadTimeout() != null) {
@@ -44,25 +63,26 @@ public class NativeApiClient implements ApiClient {
         setMethodAndBody(httpRequest, request);
 
         var requestHeaders = request.getHeaders();
-        requestHeaders.forEach((k,lv) -> lv.forEach(v -> httpRequest.header(k, v)));
+        requestHeaders.forEach((k, lv) -> lv.forEach(v -> httpRequest.header(k, v)));
 
         if (this.interceptor != null) {
             this.interceptor.accept(httpRequest);
         }
-
-        return sendRequestAndProcessResponse(httpRequest, returnType);
+        return httpRequest;
     }
 
-    private <T> ApiResponse<T> sendRequestAndProcessResponse(HttpRequest.Builder httpRequest, Class<T> returnType)
+    private <T> ApiResponse<T> sendRequestAndProcessResponse(HttpRequest.Builder httpRequest,
+                                                             ObjectReader reader)
             throws ApiException
     {
         try {
-            var response = this.getHttpClient().send(httpRequest.build(), HttpResponse.BodyHandlers.ofInputStream());
+            var response = this.getHttpClient().send(httpRequest.build(),
+                                                     HttpResponse.BodyHandlers.ofInputStream());
             if (responseInterceptor != null) {
                 responseInterceptor.accept(response);
             }
 
-            return processResponse(response, returnType);
+            return processResponse(response, reader);
         }
         catch (IOException e) {
             throw new ApiException(e);
@@ -73,10 +93,11 @@ public class NativeApiClient implements ApiClient {
         }
     }
 
-    private <T> ApiResponse<T> processResponse(HttpResponse<InputStream> response, Class<T> returnType)
+    private <T> ApiResponse<T> processResponse(HttpResponse<InputStream> response,
+                                               ObjectReader reader)
             throws IOException, ApiException
     {
-        try(var body = response.body()) {
+        try (var body = response.body()) {
             if (response.statusCode() / 100 != 2) {
                 throw new ApiException(response.statusCode(),
                                        response.headers().map(),
@@ -85,12 +106,14 @@ public class NativeApiClient implements ApiClient {
             return new ApiResponse<>(
                     response.statusCode(),
                     response.headers().map(),
-                    body == null ? null : mapper.readValue(body, returnType)
+                    body == null ? null : reader.readValue(body)
             );
         }
     }
 
-    private void setMethodAndBody(HttpRequest.Builder httpRequest, ApiRequest request) throws ApiException {
+    private void setMethodAndBody(HttpRequest.Builder httpRequest, ApiRequest request)
+            throws ApiException
+    {
         var requestBody = request.getRequestBody();
         if (requestBody == null) {
             httpRequest.method(request.getMethod(), HttpRequest.BodyPublishers.noBody());
@@ -100,11 +123,14 @@ public class NativeApiClient implements ApiClient {
         if (requestBody instanceof String) {
             httpRequest.method(request.getMethod(), HttpRequest.BodyPublishers
                     .ofString((String.valueOf(requestBody))));
-        } else {
+        }
+        else {
             try {
                 var body = mapper.writeValueAsBytes(requestBody);
-                httpRequest.method(request.getMethod(), HttpRequest.BodyPublishers.ofByteArray(body));
-            } catch (IOException e) {
+                httpRequest.method(request.getMethod(),
+                                   HttpRequest.BodyPublishers.ofByteArray(body));
+            }
+            catch (IOException e) {
                 throw new ApiException(e);
             }
         }
@@ -117,7 +143,8 @@ public class NativeApiClient implements ApiClient {
             var queryJoiner = new StringJoiner("&");
             queryParams.forEach(p -> queryJoiner.add(p.getKey() + '=' + p.getValue()));
             httpRequest.uri(URI.create(baseUri + path + '?' + queryJoiner));
-        } else {
+        }
+        else {
             httpRequest.uri(URI.create(baseUri + path));
         }
     }
--- thousandeyes-sdk-java-client/src/main/java/com/thousandeyes/api/client/ApiClient.java
+++ thousandeyes-sdk-java-client/src/main/java/com/thousandeyes/api/client/ApiClient.java
@@ -1,5 +1,12 @@
 package com.thousandeyes.api.client;
 
+import java.util.List;
+
+
+
 public interface ApiClient {
     <T> ApiResponse<T> send(ApiRequest request, Class<T> returnType) throws ApiException;
+
+    <T> ApiResponse<List<T>> sendForList(ApiRequest request, Class<T> returnType)
+            throws ApiException;
 }
